63d0147dcd4f48298f8258ef432ca9aa764f77e0
Boyd Ndonga <boydbugle@gmail.com>
2018-08-15 02:54:07 +0300

    enable function to load from localStorage

--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,39 @@
 // angular.module('starter', ['ionic'])
 angular.module('todo', ['ionic'])
 
+/**
+ * The Projects factory handles saving and loading projects
+ * from local storage, and also lets us save and load the
+ * last active project index.
+ */
+.factory('Projects', function() {
+  return {
+    all: function() {
+      var projectString = window.localStorage['projects'];
+      if(projectString) {
+        return angular.fromJson(projectString);
+      }
+      return [];
+    },
+    save: function(projects) {
+      window.localStorage['projects'] = angular.toJson(projects);
+    },
+    newProject: function(projectTitle) {
+      // Add a new project
+      return {
+        title: projectTitle,
+        tasks: []
+      };
+    },
+    getLastActiveIndex: function() {
+      return parseInt(window.localStorage['lastActiveProject']) || 0;
+    },
+    setLastActiveIndex: function(index) {
+      window.localStorage['lastActiveProject'] = index;
+    }
+  }
+})
+
 .controller('TodoCtrl', function($scope, $ionicModal) {
   // No need for testing data anymore
   $scope.tasks = [];
